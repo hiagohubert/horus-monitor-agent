7ddac7186578662c662a73ee872a4187ebe2dc93
sylar <hiagokruger@gmail.com>
2016-05-31 00:50:34 -0300

    Alterando rotina de captura de dados de memoria para melhor compatibilidade com outras distribuições Linux

--- system/SysMemory.py
+++ system/SysMemory.py
@@ -2,6 +2,7 @@ import subprocess
 import shlex
 import sys
 import os.path
+import psutil
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from entities.Memory import Memory
 
@@ -11,20 +12,16 @@ class SysMemory(object):
     def __init__(self):
         self.getMemory()
 
+    def convertToGB(self, num):
+        num = round(float(num) / 2 ** 30, 2)
+        return num
+
     def getMemory(self):
-        proc1 = subprocess.Popen(shlex.split('free -h'), stdout=subprocess.PIPE)
-        proc2 = subprocess.Popen(shlex.split('tail -n 3'), stdin=proc1.stdout,
-                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        proc3 = subprocess.Popen(shlex.split('head -n 1'), stdin=proc2.stdout,
-                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        proc1.stdout.close()
-        proc2.stdout.close()
-
-        out, err = proc3.communicate()
-
-        memory_data = out.split()
-
-        memory = Memory(float(memory_data[1].replace("G", "").replace(",", ".")), float(memory_data[2].replace("G", "").replace(",", ".")), float(memory_data[3].replace("G", "").replace(",", ".")))
+        values = psutil.virtual_memory()
+        total = self.convertToGB(values.total)
+        free = self.convertToGB(values.free)
+        used = self.convertToGB(values.used)
+        memory = Memory(total, used, free)
 
         self.memory = memory
